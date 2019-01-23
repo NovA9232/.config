8571d202f327bc6a7fefe686d8d41a703dd8f9a8
Josh <12colcloughj@lytchett.org.uk>
2019-01-23 21:22:07 +0000

    Changed surf zoom level

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
-alias sfH="setsid surf https://duckduckgo.com/"
-alias sfYT="setsid surf https://www.youtube.co.uk/"
+alias sfH="setsid surf -z 1.5 https://duckduckgo.com/"
+alias sfYT="setsid surf -z 1.5 https://www.youtube.co.uk/"
